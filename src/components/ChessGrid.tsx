1686d5e253111a5180f41c08cc6c1549256b2e52
dgrayling <daniel.grayling.2017@gmail.com>
2025-09-04 15:17:58 +0100

    Position pieces

--- src/components/ChessGrid.tsx
+++ src/components/ChessGrid.tsx
@@ -2,19 +2,162 @@ import ChessSquare from "./ChessSquare";
 
 const size = 8;
 
-type Cell = {
+type ChessSquareImmutable = {
   row: number;
   column: number;
   id: string;
 };
 
-const matrix: Cell[][] = Array.from({ length: size }, (_, rowIndex) =>
+type PieceType = "Pawn" | "Rook" | "Knight" | "Bishop" | "Queen" | "King";
+type PieceColor = "White" | "Black";
+export type ChessSquareState =
+  | { status: "empty" }
+  | { status: "occupied"; piece: { type: PieceType; color: PieceColor } };
+
+type ChessSquare = ChessSquareImmutable & {
+  state: ChessSquareState;
+};
+
+const matrix: ChessSquare[][] = Array.from({ length: size }, (_, rowIndex) =>
   Array.from({ length: size }, (_, colIndex) => ({
     row: rowIndex,
     column: colIndex,
     id: `${rowIndex}${colIndex}`,
+    state: { status: "empty" },
   }))
 );
+
+matrix[6][0].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "White" },
+};
+matrix[6][1].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "White" },
+};
+matrix[6][2].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "White" },
+};
+matrix[6][3].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "White" },
+};
+matrix[6][4].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "White" },
+};
+matrix[6][5].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "White" },
+};
+matrix[6][6].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "White" },
+};
+matrix[6][7].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "White" },
+};
+
+matrix[1][0].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "Black" },
+};
+matrix[1][1].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "Black" },
+};
+matrix[1][2].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "Black" },
+};
+matrix[1][3].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "Black" },
+};
+matrix[1][4].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "Black" },
+};
+matrix[1][5].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "Black" },
+};
+matrix[1][6].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "Black" },
+};
+matrix[1][7].state = {
+  status: "occupied",
+  piece: { type: "Pawn", color: "Black" },
+};
+
+matrix[0][0].state = {
+  status: "occupied",
+  piece: { type: "Rook", color: "White" },
+};
+matrix[0][1].state = {
+  status: "occupied",
+  piece: { type: "Knight", color: "White" },
+};
+matrix[0][2].state = {
+  status: "occupied",
+  piece: { type: "Bishop", color: "White" },
+};
+matrix[0][3].state = {
+  status: "occupied",
+  piece: { type: "Queen", color: "White" },
+};
+matrix[0][4].state = {
+  status: "occupied",
+  piece: { type: "King", color: "White" },
+};
+matrix[0][5].state = {
+  status: "occupied",
+  piece: { type: "Bishop", color: "White" },
+};
+matrix[0][6].state = {
+  status: "occupied",
+  piece: { type: "Knight", color: "White" },
+};
+matrix[0][7].state = {
+  status: "occupied",
+  piece: { type: "Rook", color: "White" },
+};
+
+matrix[7][0].state = {
+  status: "occupied",
+  piece: { type: "Rook", color: "Black" },
+};
+matrix[7][1].state = {
+  status: "occupied",
+  piece: { type: "Knight", color: "Black" },
+};
+matrix[7][2].state = {
+  status: "occupied",
+  piece: { type: "Bishop", color: "Black" },
+};
+matrix[7][3].state = {
+  status: "occupied",
+  piece: { type: "Queen", color: "Black" },
+};
+matrix[7][4].state = {
+  status: "occupied",
+  piece: { type: "King", color: "Black" },
+};
+matrix[7][5].state = {
+  status: "occupied",
+  piece: { type: "Bishop", color: "Black" },
+};
+matrix[7][6].state = {
+  status: "occupied",
+  piece: { type: "Knight", color: "Black" },
+};
+matrix[7][7].state = {
+  status: "occupied",
+  piece: { type: "Rook", color: "Black" },
+};
 
 export default function ChessGrid() {
   return (
@@ -26,7 +169,9 @@ export default function ChessGrid() {
       }}
     >
       {matrix.map((row) =>
-        row.map((cell) => <ChessSquare key={cell.id} {...cell} />)
+        row.map((cell) => (
+          <ChessSquare key={cell.id} {...cell} state={cell.state} />
+        ))
       )}
     </div>
   );
--- src/components/ChessSquare.tsx
+++ src/components/ChessSquare.tsx
@@ -1,14 +1,39 @@
-export default function ChessSquare({
-  row,
-  column,
-}: {
-  row: number;
-  column: number;
-}) {
-  return (
-    <div>
-      <p>Row:{row}</p>
-      <p>Column:{column}</p>
-    </div>
-  );
+import type { ChessSquareState } from "./ChessGrid";
+
+const filePathPrefix = "/src/assets/chess-pieces/";
+
+const pieceImages = {
+  WhitePawn: "wP.svg",
+  WhiteRook: "wR.svg",
+  WhiteKnight: "wN.svg",
+  WhiteBishop: "wB.svg",
+  WhiteQueen: "wQ.svg",
+  WhiteKing: "wK.svg",
+  BlackPawn: "bP.svg",
+  BlackRook: "bR.svg",
+  BlackKnight: "bN.svg",
+  BlackBishop: "bB.svg",
+  BlackQueen: "bQ.svg",
+  BlackKing: "bK.svg",
+} as const;
+
+function convertChessSquareStateToImage(state: ChessSquareState) {
+  switch (state.status) {
+    case "occupied":
+      return (
+        filePathPrefix + pieceImages[`${state.piece.color}${state.piece.type}`]
+      );
+    default:
+      return undefined;
+  }
+}
+
+export default function ChessSquare({ state }: { state: ChessSquareState }) {
+  const imageSrc = convertChessSquareStateToImage(state);
+
+  if (!imageSrc) {
+    return <div>Status: {state.status}</div>;
+  }
+
+  return <div>{imageSrc ? <img src={imageSrc} alt=""></img> : null}</div>;
 }
